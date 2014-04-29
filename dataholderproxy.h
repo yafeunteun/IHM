239f5c296e0002c2e7ce171cfb4c58f58f4c355d
yann feunteun <yannfeunteun@gmail.com>
2014-04-29 13:46:14 -0300

    Le proxy réalisera le filtrage des données brutes reçues sous forme de string.

--- dataholderproxy.h
+++ dataholderproxy.h
@@ -8,7 +8,7 @@ class DataHolderProxy
 public:
     static DataHolderProxy* getInstance(void);
     void addDataSet(std::initializer_list<QString&>); // verify unicity of the label
-    void addData(QVector<QPointF>& points); // verify that the size of points matches DataHolder::m_datasets' size.
+    void addData(QString& incommingRawData); // verify incomming data
     DataSet& getDataSet(const QString& label); // verify that the label exists in DataHolder::m_datasets
 private:
     static DataHolderProxy *instance;
