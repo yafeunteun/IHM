a162fe7d4433c5386ea7b9d9d00c5049e4071479
yann feunteun <yannfeunteun@gmail.com>
2014-04-29 13:43:51 -0300

    Ajout des prototypes des fonctions des classes DataHolder et DataHolderProxy.

--- dataholder.h
+++ dataholder.h
@@ -7,10 +7,15 @@ class DataHolder
 {
 public:
     static DataHolder* getInstance(void);
+    void addDataSet(std::initializer_list<QString&>);
+    void addData(QVector<QPointF>& points);
+    DataSet& getDataSet(const QString& label);
 private:
     static DataHolder *instance;
     DataHolder();
     ~DataHolder();
+
+    QVector<DataSet> m_datasets;
 };
 
 #endif // DATAHOLDER_H
--- dataholderproxy.h
+++ dataholderproxy.h
@@ -7,6 +7,9 @@ class DataHolderProxy
 {
 public:
     static DataHolderProxy* getInstance(void);
+    void addDataSet(std::initializer_list<QString&>); // verify unicity of the label
+    void addData(QVector<QPointF>& points); // verify that the size of points matches DataHolder::m_datasets' size.
+    DataSet& getDataSet(const QString& label); // verify that the label exists in DataHolder::m_datasets
 private:
     static DataHolderProxy *instance;
     DataHolderProxy();
